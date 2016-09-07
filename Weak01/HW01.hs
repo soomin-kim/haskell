a2027f8dea00643447a2ec5f47da8f43e37966b9
Soomin Kim <soomink@kaist.ac.kr>
2016-09-07 23:35:51 +0900

    First part of Week01

---
+++ Weak01/HW01.hs
@@ -0,0 +1,64 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
+lastDigit :: Integer -> Integer
+lastDigit n = n `mod` 10
+
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = n `div` 10
+
+-- Exercise 2 -----------------------------------------
+
+toRevDigitsRec :: Integer -> [Integer] -> [Integer]
+toRevDigitsRec n l
+  | n <= 0    = l
+  | otherwise = toRevDigitsRec (dropLastDigit n) ((lastDigit n) : l)
+
+toRevDigits :: Integer -> [Integer]
+toRevDigits n =
+  reverse (toRevDigitsRec n [])
+
+-- Exercise 3 -----------------------------------------
+
+doubleEveryOtherRec :: [Integer] -> [Integer] -> [Integer]
+doubleEveryOtherRec [] l2               = l2
+doubleEveryOtherRec [x] l2              = doubleEveryOtherRec [] (x : l2)
+doubleEveryOtherRec (x1 : (x2 : xs)) l2 = doubleEveryOtherRec xs ((2  *x2) : (x1 : l2))
+
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l =
+  reverse (doubleEveryOtherRec l [])
+
+-- Exercise 4 -----------------------------------------
+
+sumDigitsOne :: [Integer] -> Integer -> Integer
+sumDigitsOne [] n       = n
+sumDigitsOne (x : xs) n = sumDigitsOne xs (x + n)
+
+sumDigitsRec :: [Integer] -> Integer -> Integer
+sumDigitsRec [] n = n
+sumDigitsRec (x : xs) n = sumDigitsRec xs ((sumDigitsOne (toRevDigits x) 0) + n)
+
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits l = sumDigitsRec l 0
+
+-- Exercise 5 -----------------------------------------
+
+-- Validate a credit card number using the above functions.
+luhn :: Integer -> Bool
+luhn n = (sumDigits (doubleEveryOther (toRevDigits n))) `mod` 10 == 0
+
+-- Exercise 6 -----------------------------------------
+
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi = undefined
